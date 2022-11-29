2b6e522806ac54fca2b621618c2cc430462d3c34
Coastpure <njorogesimon465@gmail.com>
2022-11-30 00:43:58 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+
+
+function App() {
+  return (
+    <div >
+      
+
+    {/* Navbar */}
+    <Navbar />
+    {/* Add */}
+    {/* Hero */}
+    {/* Products */}
+    {/* Deals */}
+    {/* Trending */}
+    {/* Footer */}
+    </div>
+  )
+}
+
+export default App
