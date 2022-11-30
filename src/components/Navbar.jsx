763ed0ac158d9570fa922425c1db4d40cfbd6561
Coastpure <njorogesimon465@gmail.com>
2022-11-30 19:45:54 +0300

    Navbar Halfway

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import logo from '../assets/logo.png'
+import { BsCart2} from 'react-icons/bs';
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='bg-[#131921] p-4 flex items-center justify-between'>
+      {/* Left */}
+      <div className="h-10 flex">
+        <img src={logo} alt="" className='object-cover' />
+      </div>
+
+
+
+      {/* Middle */}
+      {/* Right*/}
+      <div className="relative flex items-center justify-center  text-white text-[28px]">
+       < BsCart2 />
+       <div className=" < absolute text-[16px] h-full -top-3.5 right-2.1 text-orange-400">
+        <p>0</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
