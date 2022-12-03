e88e4709cd10855613e6b6ea721f90b6b67ef343
Coastpure <njorogesimon465@gmail.com>
2022-12-03 14:25:01 +0300

    Map start

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,44 @@
 import React from 'react'
 import logo from '../assets/logo.png'
+import  kenya  from "../assets/kenya.png";
 import { BsCart2} from 'react-icons/bs';
 import { FiSearch} from 'react-icons/fi';
 import { GoSettings} from 'react-icons/go';
+import { GoLocation} from 'react-icons/go';
 
 
 const Navbar = () => {
   return (
     <div className='bg-[#131921] p-4 flex items-center justify-between'>
       {/* Left */}
-      <div className="h-10 flex">
-        <img src={logo} alt="" className='object-cover' />
+      <div className="flex items-center">
+        <div className="h-10 flex">
+          <img src={logo} alt="" className='object-cover' />
+       </div>
+       <div className="hidden text-white/70 pl-6 sm:flex items-center hover:scale-110">
+       <GoLocation className='text-[20px] mr-1'/>
+       <p className=''>Kenya 🇰🇪</p>
+       </div>
       </div>
 
-
       {/* Middle */}
-    <div className="hidden sm:flex relative overflow-hidden">
-      <input type="search" placeholder='Search' className='w-[20rem] py-2 rounded-full pl-12 placeholder:text-[#131921] outline-0'/>
-      <button className='absolute bg-orange-400 h-full w-12 right-0 flex items-center justify-center rounded-r-full'>
-         <FiSearch /> 
-      </button>
-      <button className='absolute text-[#131921] bg-[#f3f3f3]  h-full w-12  flex items-center justify-center rounded-l-full'>
-         <GoSettings /> 
-      </button>
+    <div className="">
+      {/* iNPUT */}
+      <div className="hidden sm:flex relative">
+        <input type="search" placeholder='Search' className='w-[20rem] py-2 rounded-full pl-12 placeholder:text-  [#131921] outline-0'/>
+        <button className='absolute bg-orange-400 h-full w-12 right-0 flex items-center justify-center  rounded-r-full'>
+          <FiSearch /> 
+        </button>
+        <button className='absolute text-[#131921] bg-[#f3f3f3]  h-full w-12  flex items-center justify-center  rounded-l-full'>
+          <GoSettings /> 
+       </button>
+     </div>
+     {/* FLAG */}
+     <div className="">
+      <img src={kenya} alt="" srcset="" />
+     </div>
     </div>
+    
 
 
 
